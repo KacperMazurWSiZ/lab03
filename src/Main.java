e62e02e320363741a1fdfc7c57b4edcadaa63479
KacperMazurWSiZ <w66057@student.wsiz.edu.pl>
2022-11-20 12:54:14 +0100

    punkt dziedziczenie

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,6 @@ public class Main {
 //        b2.info();
 
 
-        
+
     }
 }
---
+++ src/Punkt.java
@@ -0,0 +1,45 @@
+public class Punkt {
+
+    public double x,y;
+
+    public Punkt(double x) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Punkt(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void  zeruj(){
+        x = 0.0;
+        y = 0.0;
+    }
+
+    public void opis(){
+        System.out.println("Pkt o wspol. [x,y]" + x + " " + y);
+    }
+
+    public void przesun(double x, double y){
+        this.x += x;
+        this.y += y;
+
+    }
+}
